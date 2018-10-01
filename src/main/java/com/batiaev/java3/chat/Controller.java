7db6a02ac238e1d42431b5881b1aba0632f3508c
Anton Batiaev <a.batyaev@gmail.com>
2018-10-01 20:28:24 +0300

    add homework 3

--- src/main/java/com/batiaev/java3/chat/Controller.java
+++ src/main/java/com/batiaev/java3/chat/Controller.java
@@ -3,6 +3,8 @@ package com.batiaev.java3.chat;
 public interface Controller {
     void sendMessage(String msg);
 
+    void storeMessage(String msg);
+
     void closeConnection();
 
     void showUI(ClientUI clientUI);
