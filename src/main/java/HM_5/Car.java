05313e10fbef2dd8a0e5b2e5e4a821422dbb5d6b
Daridang <daridang@gmail.com>
2018-10-07 05:28:44 +0100

    HomeWork_5 Race

---
+++ src/main/java/HM_5/Car.java
@@ -0,0 +1,53 @@
+package HM_5;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+
+public class Car implements Runnable {
+    private static int CARS_COUNT;
+
+    static {
+        CARS_COUNT = 0;
+    }
+
+    private Race race;
+    private int speed;
+    private String name;
+    private CountDownLatch latch;
+    private CyclicBarrier[] barrier;
+
+    String getName() {
+        return name;
+    }
+
+    int getSpeed() {
+        return speed;
+    }
+
+    Car(Race race, int speed, CountDownLatch latch, CyclicBarrier... b) {
+        this.race = race;
+        this.speed = speed;
+        this.barrier = b;
+        this.latch = latch;
+        CARS_COUNT++;
+        this.name = "Участник #" + CARS_COUNT;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println(this.name + " готовится");
+            Thread.sleep(500 + (int) (Math.random() * 800));
+            System.out.println(this.name + " готов");
+            barrier[0].await();
+            barrier[1].await();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < race.getStages().size(); i++) {
+            race.getStages().get(i).go(this);
+        }
+        latch.countDown();
+    }
+}
