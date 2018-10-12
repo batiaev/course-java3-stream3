0f84118698647f13de8f3438071227ad6b9dfa61
Maklakov Sergey <maklakov.sergey@hotmail.com>
2018-10-12 16:05:18 +0300

    dz7

---
+++ src/main/java/com/batiaev/java3/lesson7/homework/testing/BeforeSuite.java
@@ -0,0 +1,11 @@
+package com.batiaev.java3.lesson7.homework.testing;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface BeforeSuite {
+}
---
+++ src/main/java/com/batiaev/java3/lesson7/homework/testing/Test.java
@@ -0,0 +1,12 @@
+package com.batiaev.java3.lesson7.homework.testing;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Test {
+    int priority() default 99999;
+}
